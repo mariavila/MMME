d3fc4517994124ca48defe351d1b23724c9160d0
martin.dansc <martin.dansc@gmail.com>
2017-03-18 12:50:44 +0100

    Added state schema

--- server/main.js
+++ server/main.js
@@ -4,10 +4,15 @@ var http = require('http').Server(app);
 var bodyParser = require('body-parser');
 var io = require('socket.io')(http);
 
+var state = requiere('./state');
+var db = require('./db');
+
 app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+db.connect();
 
 //Server
 http.listen(3000, function(){
---
+++ server/migrations/schema.sql
@@ -0,0 +1,8 @@
+-- UP
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `idClient` TEXT NOT NULL PRIMARY KEY,
+  `balance` INTEGER
+);
+
+-- Down
