59d1e630837603a06ab90174f09504f51801c6ef
mariavila <mariava.1213@gmail.com>
2017-03-18 16:47:49 +0100

    inicial

--- server/main.js
+++ server/main.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var bodyParser = require('body-parser');
-var io = require('socket.io')(http);
 
 var state = requiere('./state');
 var db = require('./db');
@@ -18,3 +17,25 @@ db.connect();
 http.listen(3000, function(){
  	console.log('listening on *:3000');
 });
+
+app.get('/login', function(req,res){
+	var user_id = req.param('id');
+	///////////getUserInfo(user_id);
+});
+
+app.get('/initRoute', function(req, res){
+	var pos_ini = req.param('pos_ini');
+	var pos_fi = req.param('pos_fi');
+	var user_id = req.param('id');
+	/*MARC calcula la ruta*/
+	-->send routes to frontend 
+});
+
+app.get('/chooseRoute', function(req, res){
+	var route = req.param('route');
+	var id = req.param('id');
+	var id = req.param('earned');
+	//////////////
+});
+
+app.get()
