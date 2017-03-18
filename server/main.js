ee8a9cae9023552c33761b74ca558b5f8840c6b2
martin.dansc <martin.dansc@gmail.com>
2017-03-18 11:35:28 +0100

    initial commit

---
+++ server/main.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var bodyParser = require('body-parser');
+var io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Server
+http.listen(3000, function(){
+ 	console.log('listening on *:3000');
+});
+
+//IO
+io.on('connection', function(socket){
+	
+	var id = socket.id;
+	
+	socket.on('disconnect', function() {
+			
+	});
+
+	//Add connection TODO
+});
